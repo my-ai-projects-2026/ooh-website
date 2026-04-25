d4bc16d8e8a08000ac28252ef9846a6a68e5acb6
amercado <arnold.mercado@hotmail.com>
2026-04-25 18:15:54 +0800

    feat: remove runtime declaration from studio page

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -13,7 +13,7 @@ import config from '../../../../sanity.config'
 export const dynamic = 'force-static'
 
 export { metadata, viewport } from 'next-sanity/studio'
-export const runtime = 'edge';
+
 export default function StudioPage() {
   return <NextStudio config={config} />
 }
