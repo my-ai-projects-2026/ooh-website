09f461547db6fd42fbd0c198d60205c2bc240044
amercado <arnold.mercado@hotmail.com>
2026-04-25 12:52:06 +0800

    feat: set runtime to edge in layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
 
+export const runtime = 'edge'
+
 const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
